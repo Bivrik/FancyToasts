a17d3410058a04a085c8250b322a836bf10dcbf3
Bivrik <markvaselkof@yandex.ru>
2025-12-05 23:49:20 +0300

    Repetable quests handling

--- fabric/src/main/java/net/bivrik/fancytoasts/compat/FTBQuestsCompat.java
+++ fabric/src/main/java/net/bivrik/fancytoasts/compat/FTBQuestsCompat.java
@@ -8,23 +8,23 @@ import dev.ftb.mods.ftbquests.client.gui.ToastQuestObject;
 import dev.ftb.mods.ftbquests.item.FTBQuestsItems;
 import dev.ftb.mods.ftbquests.quest.Quest;
 import dev.ftb.mods.ftbquests.quest.QuestObjectBase;
-import net.bivrik.fancytoasts.core.Debug;
+import net.bivrik.fancytoasts.core.Constants;
 import net.bivrik.fancytoasts.platform.utility.FancyToastType;
 import net.bivrik.fancytoasts.platform.utility.QuestToastDisplayInfo;
 import net.bivrik.fancytoasts.platform.utility.ResourceLocations;
 import net.bivrik.fancytoasts.platform.utility.ToastDisplayInfo;
 import net.minecraft.client.gui.components.toasts.Toast;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class FTBQuestsCompat {
+    private static final Map<Long, Long> REPEATABLE_QUESTS = new WeakHashMap<>(4);
+    private static final int MINUTE = 1000 * 60;
+    private static final int TIME = MINUTE;
+
     public static boolean isQuest(Toast toast) {
         return toast instanceof ToastQuestObject;
     }
@@ -39,12 +39,29 @@ public class FTBQuestsCompat {
         List<ItemStack> icons = new ArrayList<>(1);
 
         // Try to find a description for quest. Not really efficient, but I can't think of any other solution
+        Quest quest = null;
         for (QuestObjectBase questObject : ClientQuestFile.INSTANCE.getAllObjects()) {
             if (questObject.getTitle().equals(title)) {
-                Quest quest = ClientQuestFile.INSTANCE.getQuest(questObject.id);
-                if (quest != null) {
-                    description = quest.getSubtitle();
+                Quest foundQuest = ClientQuestFile.INSTANCE.getQuest(questObject.id);
+                if (foundQuest != null) {
+                    quest = foundQuest;
+                    description = foundQuest.getSubtitle();
                     break;
+                }
+            }
+        }
+
+        // Handle repeatable quests
+        long currentTime = System.currentTimeMillis();
+        if (quest != null) {
+            Long id = quest.id;
+            REPEATABLE_QUESTS.entrySet().removeIf(entry -> currentTime - entry.getValue() > TIME);
+
+            if (quest.canBeRepeated()) {
+                if (REPEATABLE_QUESTS.containsKey(id)) {
+                    return null;
+                } else {
+                    REPEATABLE_QUESTS.put(id, currentTime);
                 }
             }
         }
@@ -55,13 +72,15 @@ public class FTBQuestsCompat {
             icons.add(itemIcon.getStack());
         } else if (questIcon instanceof IconAnimation iconAnimation) {
             for (Icon icon : iconAnimation.list) {
-                icons.add(((ItemIcon) icon).getStack());
+                if (icon instanceof ItemIcon itemIcon) {
+                    icons.add(itemIcon.getStack());
+                }
             }
         }
 
         // If there is custom texture just replace it with a FTBQuests' book. Or just in general other edge cases. It's better to have something than nothing, I guess
         if (icons.isEmpty()) {
-            Item item = FTBQuestsItems.ITEMS.getRegistrar().get(ResourceLocations.withNamespaceAndPath("ftbquests", "book"));
+            Item item = FTBQuestsItems.ITEMS.getRegistrar().get(ResourceLocations.withNamespaceAndPath(Constants.Compatibilities.FTB_QUESTS_ID, "book"));
             if (item != null) {
                 ItemStack icon = new ItemStack(item);
                 icons.add(icon);
