442419867bcaf607d44d1e820e3b95eb130127d9
Bivrik <markvaselkof@yandex.ru>
2026-04-25 23:47:03 +0300

    Chore: changed sound for task type advancement

--- common/src/main/java/net/bivrik/fancytoasts/client/config/data/ToastConfigData.java
+++ common/src/main/java/net/bivrik/fancytoasts/client/config/data/ToastConfigData.java
@@ -32,7 +32,7 @@ public class ToastConfigData extends ConfigData {
 
     public ToastConfigData() {
         this(DefaultLocations.Textures.VANILLA, DefaultLocations.Animations.STANDARD, Map.of(
-                FancyToastType.TASK, SoundEvents.ALLAY_AMBIENT_WITH_ITEM.location(),
+                FancyToastType.TASK, SoundEvents.NOTE_BLOCK_CHIME.value().location(),
                 FancyToastType.GOAL, SoundEvents.FIREWORK_ROCKET_TWINKLE_FAR.location(),
                 FancyToastType.CHALLENGE, SoundEvents.UI_TOAST_CHALLENGE_COMPLETE.location())
         );
