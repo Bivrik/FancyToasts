b1c2668fe5f73f6171b36d5429018f36186f96bc
Bivrik <markvaselkof@yandex.ru>
2026-08-22 05:14:02 +0300

    Cleaned up Components and moved extractKey(Component) by Furglitch here

--- common/src/main/java/net/bivrik/fancytoasts/platform/utility/Components.java
+++ common/src/main/java/net/bivrik/fancytoasts/platform/utility/Components.java
@@ -4,58 +4,31 @@ import net.bivrik.fancytoasts.core.Constants;
 import net.minecraft.network.chat.Component;
 
 public class Components {
-    private static final String DEFAULT_ID = Constants.MOD_ID;
-
-    private static Component getTranslatableComponent(String modId, String name, TranslatableType type) {
-        StringBuilder translationKey = new StringBuilder(DEFAULT_ID);
-        if (modId != null && modId.compareTo(DEFAULT_ID) != 0) {
-            translationKey.append(".").append(modId);
-        }
-        translationKey.append(".").append(type.getName()).append(".toast.").append(name);
-
-        return Component.translatable(translationKey.toString());
+    public static Component of(String key) {
+        return Component.translatable(stringOf(key));
     }
 
-    public static Component of(String path) {
-        return Component.translatable(stringOf(path));
-    }
-
-    public static String stringOf(String path) {
-        return DEFAULT_ID + "." + path;
+    public static String stringOf(String key) {
+        return Constants.MOD_ID + "." + key;
     }
 
     /**
-     * Easier way to get translatable name for textures
-     * @param modId is for texture's path. If 'null' or 'fancytoasts' then it returns default
-     * @param name is for texture's name/identifier
-     * @return fancytoasts.'modId'.textures.'displayName'
+     * Extracts key for translation from {@link Component}.
+     * @param translatable translatable component
+     * @return string key used for translation
+     * @author Furglitch
      */
-    public static Component translatableTexture(String modId, String name) {
-        return getTranslatableComponent(modId, name, TranslatableType.TEXTURE);
-    }
+    public static String extractKey(Component translatable)
+    {
+        String s = translatable.toString();
+        String marker = "key='";
+        int startIndex = s.indexOf(marker);
+        if (startIndex == -1) return null;
 
-    /**
-     *Easier way to get translatable name for animations
-     * @param modId is for animation's path. If 'null' or 'fancytoasts' then it returns default
-     * @param name is for animation's name/identifier
-     * @return fancytoasts.'modId'.animations.'displayName'
-     */
-    public static Component translatableAnimation(String modId, String name) {
-        return getTranslatableComponent(modId, name, TranslatableType.ANIMATION);
-    }
+        startIndex += marker.length();
+        int endIndex = s.indexOf("\'", startIndex);
+        if (endIndex == -1) return null;
 
-    private enum TranslatableType {
-        ANIMATION("animations"),
-        TEXTURE("textures");
-
-        private final String name;
-
-        TranslatableType(String name) {
-            this.name = name;
-        }
-
-        public String getName() {
-            return name;
-        }
+        return s.substring(startIndex, endIndex);
     }
 }
