0b2694345f107c9ad95646825b91ac17607f8746
Bivrik <markvaselkof@yandex.ru>
2026-08-23 08:08:39 +0300

    Updated main screen and added image buttons for my social links

--- common/src/main/java/net/bivrik/fancytoasts/client/gui/screen/FancyToastsScreen.java
+++ common/src/main/java/net/bivrik/fancytoasts/client/gui/screen/FancyToastsScreen.java
@@ -4,10 +4,10 @@ import net.bivrik.fancytoasts.FancyToasts;
 import net.bivrik.fancytoasts.core.Color;
 import net.bivrik.fancytoasts.platform.utility.Components;
 import net.bivrik.fancytoasts.platform.utility.GuiContext;
+import net.bivrik.fancytoasts.platform.utility.ResourceLocations;
 import net.minecraft.Util;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.components.Button;
-import net.minecraft.client.gui.components.PlainTextButton;
+import net.minecraft.client.gui.components.*;
 import net.minecraft.client.gui.screens.ConfirmLinkScreen;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.CommonComponents;
@@ -15,21 +15,29 @@ import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.NotNull;
 
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.List;
 
 import static net.bivrik.fancytoasts.client.gui.LayoutValues.*;
 
 public class FancyToastsScreen extends UniversalScreen {
-    private static final int TITLE_BUTTON_WIDTH = 180;
+    private static final int TITLE_BUTTON_WIDTH = 200;
     private static final int HALF_TITLE_BUTTON_WIDTH = TITLE_BUTTON_WIDTH / 2;
 
     private static final Component TITLE = Component.literal("Fancy Toasts");
-    private static final Component SUPPORT_LABEL = Components.of("label.support");
     private static final Component TOAST_SETTINGS = Components.of("gui.toast_settings");
     private static final Component GENERAL_SETTINGS = Components.of("gui.general_settings");
     private static final Component TOASTS_FILTERING = Components.of("gui.toasts_filtering");
     private static final Component CREDITS = Components.of("gui.credits");
+    private static final Component GITHUB_LABEL = Components.of("label.creator_note");
+    private static final Component DISCORD_TOOLTIP = Components.of("tooltip.discord");
+    private static final Component BOOSTY_TOOLTIP = Components.of("tooltip.boosty");
+    private static final Component YOUTUBE_TOOLTIP = Components.of("tooltip.youtube");
 
+    private static final URI GITHUB_URI = URI.create("https://github.com/Bivrik");
+    private static final URI DISCORD_URI = URI.create("https://discord.gg/9XuRDgbbZe");
     private static final URI BOOSTY_URI = URI.create("https://boosty.to/bivrik");
+    private static final URI YOUTUBE_URI = URI.create("https://www.youtube.com/@modsEnjoyer");
 
     private final String splash;
 
@@ -50,7 +58,6 @@ public class FancyToastsScreen extends UniversalScreen {
     protected void init() {
         int xCenter = this.width / 2 - HALF_TITLE_BUTTON_WIDTH;
         int yCenter = this.height / 2;
-        int supportButtonWidth = this.font.width(SUPPORT_LABEL);
 
         toastConfigButton = this.addFWidget(this.createButton(TOAST_SETTINGS, button -> openToastConfigScreen(),
                 xCenter, yCenter - BUTTON_HEIGHT - PADDING));
@@ -67,13 +74,41 @@ public class FancyToastsScreen extends UniversalScreen {
         backButton = this.addFWidget(this.createButton(CommonComponents.GUI_BACK, button -> this.toParentScreen(),
                 xCenter, this.height - BUTTON_HEIGHT - 16));
 
-        Button.OnPress supportButtonAction = ConfirmLinkScreen.confirmLink(this, BOOSTY_URI);
-        supportButton = this.addFWidget(new PlainTextButton(this.width - supportButtonWidth - 1, this.height - 10, supportButtonWidth, 9, SUPPORT_LABEL, supportButtonAction, this.font));
+        List<ImageButton> linkButtons = new ArrayList<>();
+
+        linkButtons.add(createLinkButton("links/discord", "links/discord_hover",
+                DISCORD_URI, DISCORD_TOOLTIP));
+
+        linkButtons.add(createLinkButton("links/boosty", "links/boosty_hover",
+                BOOSTY_URI, BOOSTY_TOOLTIP));
+
+        linkButtons.add(createLinkButton("links/youtube", "links/youtube_hover",
+                YOUTUBE_URI, YOUTUBE_TOOLTIP));
+
+        int x = toastConfigButton.getRight() + PADDING;
+        int y = toastConfigButton.getY();
+        for (ImageButton button : linkButtons) {
+            button.setPosition(x, y);
+            addFWidget(button);
+            y += button.getHeight() + 5;
+        }
+
+        int supportButtonWidth = this.font.width(GITHUB_LABEL);
+        Button.OnPress openGithubAction = ConfirmLinkScreen.confirmLink(this, GITHUB_URI);
+        supportButton = new PlainTextButton(this.width - supportButtonWidth - 2, this.height - 9 - 1, supportButtonWidth, 9, GITHUB_LABEL, openGithubAction, this.font);
+        addFWidget(supportButton);
     }
 
     @Override
     protected Button createButton(Component label, Button.OnPress action, int x, int y) {
         return super.createButton(label, action, x, y, TITLE_BUTTON_WIDTH, BUTTON_HEIGHT);
+    }
+
+    private ImageButton createLinkButton(String icon, String iconHovered, URI link, Component tooltip) {
+        Button.OnPress action = ConfirmLinkScreen.confirmLink(this, link);
+        ImageButton button = new ImageButton(0, 0, 18, 18, new WidgetSprites(ResourceLocations.of(icon), ResourceLocations.of(iconHovered)), action);
+        button.setTooltip(Tooltip.create(tooltip));
+        return button;
     }
 
     private void openToastConfigScreen() {
