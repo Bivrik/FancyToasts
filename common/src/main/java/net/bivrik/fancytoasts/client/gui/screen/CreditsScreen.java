0b4294dbcb5b1f34866aefa3c1edb8463a5287f8
Bivrik <markvaselkof@yandex.ru>
2026-08-23 07:42:45 +0300

    Updated credits screen

--- common/src/main/java/net/bivrik/fancytoasts/client/gui/screen/CreditsScreen.java
+++ common/src/main/java/net/bivrik/fancytoasts/client/gui/screen/CreditsScreen.java
@@ -1,42 +1,57 @@
 package net.bivrik.fancytoasts.client.gui.screen;
 
+import com.mojang.blaze3d.platform.GlStateManager;
 import com.mojang.blaze3d.systems.RenderSystem;
-import net.bivrik.fancytoasts.client.gui.CreditsList;
-import net.bivrik.fancytoasts.core.manager.CreditsManager;
-import net.bivrik.fancytoasts.platform.utility.GuiContext;
-import net.bivrik.fancytoasts.utility.TextureUV;
+import net.bivrik.fancytoasts.FancyToasts;
+import net.bivrik.fancytoasts.client.credits.CreditsList;
+import net.bivrik.fancytoasts.client.credits.CreditsManager;
+import net.bivrik.fancytoasts.platform.utility.Components;
+import net.bivrik.fancytoasts.platform.utility.ResourceLocations;
+import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
-import net.minecraft.resources.Identifier;
+import net.minecraft.resources.ResourceLocation;
 import org.jetbrains.annotations.NotNull;
 
-import static net.bivrik.fancytoasts.client.gui.LayoutValues.PADDING;
-
 public class CreditsScreen extends UniversalScreen {
-    private static final Identifier VIGNETTE_LOCATION = Identifiers.fromMinecraft("textures/misc/credits_vignette.png");
+    private static final ResourceLocation VIGNETTE_LOCATION = ResourceLocations.fromMinecraft("textures/misc/credits_vignette.png");
+    private static final Component TITLE = Components.of("gui.credits");
 
     private final CreditsManager.CreditsData creditsData;
 
     private CreditsList creditsList;
 
     public CreditsScreen(Screen parent) {
-        super(Component.empty(), parent);
+        super(TITLE, parent);
 
         this.creditsData = FancyToasts.getInstance().getCreditsManager().getCredits();
     }
 
     @Override
     protected void init() {
-        creditsList = this.addFRenderable(new CreditsList(this.minecraft, this.width , this.height, PADDING, 0, creditsData));
-        this.setFocused(creditsList);
+        int offset = 12 + 9 + 12;
+        creditsList = new CreditsList(this.minecraft, this.width, this.height - offset * 2, 8, offset, creditsData);
+        addFRenderable(creditsList);
+        setFocused(creditsList);
     }
 
     @Override
-    public void extractRenderState(@NotNull GuiGraphicsExtractor guiGraphicsExtractor, int mouseX, int mouseY, float partialTick) {
-        super.extractRenderState(guiGraphicsExtractor, mouseX, mouseY, partialTick);
+    public void render(@NotNull GuiGraphics guiGraphics, int mouseX, int mouseY, float partialTick) {
+        renderBackground(guiGraphics, mouseX, mouseY, partialTick);
 
-        new GuiContext(guiGraphicsExtractor).drawTexture(RenderPipelines.VIGNETTE, VIGNETTE_LOCATION, 0, 0, this.width, this.height, TextureUV.ZERO, this.width, this.height);
+        int offset = 12 + 9 + 12;
+        int width = this.width;
+        int height = this.height - offset * 2;
+        RenderSystem.enableBlend();
+        RenderSystem.blendFunc(GlStateManager.SourceFactor.ZERO, GlStateManager.DestFactor.ONE_MINUS_SRC_COLOR);
+        guiGraphics.blit(VIGNETTE_LOCATION, 0, 0, 0, 0, this.width, this.height, this.width, this.height);
+        guiGraphics.blit(VIGNETTE_LOCATION, 0, offset, 0, 0, width, height, width, height);
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.disableBlend();
 
         creditsList.scroll();
+
+        drawRenderables(guiGraphics, mouseX, mouseY, partialTick);
+        drawTitle(guiGraphics);
     }
 }
