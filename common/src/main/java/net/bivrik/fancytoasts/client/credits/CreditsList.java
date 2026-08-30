dde53acf7bd46ce96908c255f983ca79bb6f5c9e
Bivrik <markvaselkof@yandex.ru>
2026-08-30 09:31:37 +0300

    Added fallback transformation for nonsupported categories in CreditsList

--- common/src/main/java/net/bivrik/fancytoasts/client/credits/CreditsList.java
+++ common/src/main/java/net/bivrik/fancytoasts/client/credits/CreditsList.java
@@ -1,5 +1,6 @@
 package net.bivrik.fancytoasts.client.credits;
 
+import net.bivrik.fancytoasts.client.toast.DisplayData;
 import net.bivrik.fancytoasts.core.Color;
 import net.bivrik.fancytoasts.core.Constants;
 import net.bivrik.fancytoasts.platform.utility.Components;
@@ -9,6 +10,7 @@ import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.client.gui.components.AbstractSelectionList;
 import net.minecraft.client.gui.narration.NarrationElementOutput;
 import net.minecraft.client.input.KeyEvent;
+import net.minecraft.locale.Language;
 import net.minecraft.network.chat.Component;
 import org.jetbrains.annotations.NotNull;
 import org.lwjgl.glfw.GLFW;
@@ -131,7 +133,7 @@ public class CreditsList extends AbstractSelectionList<CreditsList.Entry> {
         public Entry(CreditsList parentList, String content) {
             this.parentList = parentList;
             this.content = content;
-            this.font = this.parentList.minecraft.font;
+            this.font = parentList.minecraft.font;
         }
 
         @Override
@@ -145,13 +147,18 @@ public class CreditsList extends AbstractSelectionList<CreditsList.Entry> {
         public CategoryEntry(CreditsList parentList, String content) {
             super(parentList, content);
 
-            this.displayName = Component.translatable(Constants.MOD_ID + ".label." + this.content);
+            String translationKey = Constants.MOD_ID + ".label." + content;
+            if (Language.getInstance().has(translationKey)) {
+                this.displayName = Component.translatable(translationKey);
+            } else {
+                this.displayName = Component.literal(DisplayData.getVisualAppealingString(content));
+            }
             this.xCenter = parentList.getWidth() / 2;
         }
 
         @Override
         public void extractContent(@NotNull GuiGraphicsExtractor graphics, int mouseX, int mouseY, boolean isHovering, float partialTick) {
-            graphics.centeredText(font, displayName, xCenter, getY(), Color.YELLOW.getARGB());
+            graphics.centeredText(this.font, displayName, xCenter, getY(), Color.YELLOW.getARGB());
         }
     }
 
@@ -168,7 +175,7 @@ public class CreditsList extends AbstractSelectionList<CreditsList.Entry> {
         public UserEntry(CreditsList parentList, String content, String annotation) {
             super(parentList, content);
             this.annotation = annotation;
-            this.isValidAnnotation = this.annotation != null && !this.annotation.isEmpty();
+            this.isValidAnnotation = annotation != null && !annotation.isEmpty();
         }
 
         @Override
